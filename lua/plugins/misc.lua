7294036f4db51e3f10f2afe8c88a963a70c6318e
Zijia Xiong <isxiongzj@gmail.com>
2024-11-27 19:46:36 +0000

    1

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -132,4 +132,12 @@ return {
       -- },
     },
   },
+  {
+    "anurag3301/nvim-platformio.lua",
+    dependencies = {
+      { "akinsho/nvim-toggleterm.lua" },
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+  },
 }
