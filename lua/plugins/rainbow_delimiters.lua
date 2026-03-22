8c801b7d747521d7b2e7741a1ac9a664423eb648
Zijia <isxiongzj@gmail.com>
2026-03-22 00:32:02 +0000

    Fix BufReadPost error for xxd buffers

--- lua/plugins/rainbow_delimiters.lua
+++ lua/plugins/rainbow_delimiters.lua
@@ -1,3 +1,15 @@
 return {
-  { "https://gitlab.com/HiPhish/rainbow-delimiters.nvim" },
+  {
+    "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+    init = function()
+      local existing = vim.g.rainbow_delimiters or {}
+
+      vim.g.rainbow_delimiters = vim.tbl_deep_extend("force", existing, {
+        blacklist = { "neo-tree", "neo-tree-preview", "xxd" },
+        condition = function(bufnr)
+          return vim.bo[bufnr].buftype == ""
+        end,
+      })
+    end,
+  },
 }
